4b3d2966b28a54858f153ea230efb2eab11d5ffc
Moises Astudillo <bruma_moy@hotmail.com>
2020-05-25 01:55:33 -0500

    Validacion del Formulario

--- src/main/java/com/example/HolaSpring/web/ControladorInicio.java
+++ src/main/java/com/example/HolaSpring/web/ControladorInicio.java
@@ -1,10 +1,12 @@
 package com.example.HolaSpring.web;
 import com.example.HolaSpring.domain.Persona;
 import com.example.HolaSpring.servicio.PersonaService;
+import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -32,7 +34,10 @@ public class ControladorInicio {
     }
     
     @PostMapping ("/guardar")
-    public String guardar(Persona persona){
+    public String guardar(@Valid Persona persona, Errors errors){
+        if(errors.hasErrors()){
+            return "modificar";
+        }
         personaService.guardar(persona);
         return "redirect:/";
     }
